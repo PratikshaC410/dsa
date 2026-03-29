84b4059c99bc598ef521e3923df0e6711004f486
PratikshaC410 <pratiksha42719@gmail.com>
2026-03-29 19:16:19 +0530

    product of array except itself

---
+++ ARRAYS/productofarrexceptitself.cpp
@@ -0,0 +1,27 @@
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+#include <bits/std++.h>
+vector<int> productExceptSelf(vector<int> &nums)
+{
+    int n = nums.size();
+    vector<int> answer(n, 1);
+
+    // First pass: prefix products (left to right)
+    int prefix = 1;
+    for (int i = 0; i < n; i++)
+    {
+        answer[i] = prefix;
+        prefix *= nums[i];
+    }
+
+    // Second pass: multiply by suffix products (right to left)
+    int suffix = 1;
+    for (int i = n - 1; i >= 0; i--)
+    {
+        answer[i] *= suffix;
+        suffix *= nums[i];
+    }
+
+    return answer;
+}
