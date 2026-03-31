808d32d78f6f41d9552cc9cb135b50732e9e0548
PratikshaC410 <pratiksha42719@gmail.com>
2026-03-31 13:53:32 +0530

    container with most water using 2 pointers

---
+++ ARRAYS/containerwithmostwater.cpp
@@ -0,0 +1,33 @@
+// You are given an integer array height of length n.There are n vertical
+// lines drawn such that the two endpoints of the ith line
+// are(i, 0) and
+//  (i, height[i]).Find two lines that together with the x - axis form a container,
+// such that the container contains the most water.Return the maximum amount of water a
+// container can store.Notice that you may not slant the container.
+
+#include <bits/stdc++.h>
+
+class Solution
+{
+public:
+    int maxArea(vector<int> &height)
+    {
+        int n = height.size();
+        int left = 0, right = n - 1;
+        int maxwater = INT_MIN;
+        while (left <= right)
+        {
+            int width = right - left;
+            int h = min(height[left], height[right]);
+            int water = width * h;
+            maxwater = max(water, maxwater);
+            if (height[left] < height[right])
+            {
+                left++;
+            }
+            else
+                right--;
+        }
+        return maxwater;
+    }
+};
