4fb51cf17a7da158223d4b53417fd402a1064fee
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-02 22:54:11 +0530

    identical binary trees

---
+++ trees/identicaltrees.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *left;
+    Node *right;
+
+    Node(int val) : data(val), left(nullptr), right(nullptr) {}
+};
+
+class Solution
+{
+public:
+    bool isIdentical(Node *node1, Node *node2)
+    {
+        if (node1 == NULL && node2 == NULL)
+        {
+            return true;
+        }
+        if (node1 == NULL || node2 == NULL)
+        {
+            return false;
+        }
+
+        return ((node1->data == node2->data) && isIdentical(node1->left, node2->left) && isIdentical(node1->right, node2->right));
+    }
+};
