a2e2670a825f5153ab4ff9311db6a155fbbaad67
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-08 21:41:26 +0530

    changes made

--- ARRAYS/incsubarr.cpp
+++ ARRAYS/incsubarr.cpp
@@ -1,65 +1,29 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+
 using namespace std;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    vector<int> a(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> a[i];
+    }
+
+    long long result = (long long)n - a[m - 1] + 1;
+    cout << result << endl;
+}
 
 int main()
 {
-
     int t;
     cin >> t;
     while (t--)
     {
-        int n, m;
-        cin >> n >> m;
-
-        vector<int> a(m);
-        for (int i = 0; i < m; i++)
-            cin >> a[i];
-
-        bool ok = true;
-        for (int i = 0; i < m - 1; i++)
-        {
-            if (!(a[i + 1] == a[i] + 1 || a[i + 1] == 1))
-            {
-                ok = false;
-                break;
-            }
-        }
-
-        if (!ok)
-        {
-            cout << 0 << '\n';
-            continue;
-        }
-
-        vector<int> seg;
-        int len = 1;
-        for (int i = 1; i < m; i++)
-        {
-            if (a[i] == 1)
-            {
-                seg.push_back(len);
-                len = 1;
-            }
-            else
-            {
-                len++;
-            }
-        }
-        seg.push_back(len);
-
-        int L = 0;
-        for (int i = 0; i < (int)seg.size(); i++)
-        {
-            L = max(L, seg[i] - i);
-        }
-
-        L = max(L, a[0]);
-
-        int R = n - ((int)seg.size() - 1);
-
-        int ans = max(0, R - L + 1);
-        cout << ans << '\n';
+        solve();
     }
-
     return 0;
 }
