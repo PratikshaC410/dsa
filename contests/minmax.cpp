34309f453d0de50f60ce69bf77cdbca1f1b40880
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-12 21:43:47 +0530

    changes made

--- contests/minmax.cpp
+++ contests/minmax.cpp
@@ -1,25 +1,35 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
+
+void solve()
+{
+    int x;
+    if (!(cin >> x))
+        return;
+
+    if (x < 67)
+    {
+        cout << x + 1 << endl;
+    }
+    else
+    {
+        cout << 67 << endl;
+    }
+}
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
     int t;
-    cin >> t;
-
+    if (!(cin >> t))
+        return 0;
     while (t--)
     {
-        int x;
-        cin >> x;
-        if (x == 67)
-        {
-            cout << 66 << "\n";
-        }
-        else
-        {
-            cout << x + 1 << "\n";
-        }
+        solve();
     }
-
     return 0;
 }
