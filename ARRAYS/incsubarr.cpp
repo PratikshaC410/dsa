a62e19f778edfa055529b10ea3d15bc61e6f0c37
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-12 21:43:15 +0530

    increamental subaaray

--- ARRAYS/incsubarr.cpp
+++ ARRAYS/incsubarr.cpp
@@ -5,22 +5,44 @@ using namespace std;
 
 void solve()
 {
-    int n, m;
-    cin >> n >> m;
-    vector<int> a(m);
+    long long n, m;
+    if (!(cin >> n >> m))
+        return;
+
+    vector<long long> a(static_cast<size_t>(m));
+    bool has_restart = false;
+
     for (int i = 0; i < m; i++)
     {
-        cin >> a[i];
+        cin >> a[static_cast<size_t>(i)];
+        if (i > 0)
+        {
+            if (a[static_cast<size_t>(i)] != a[static_cast<size_t>(i - 1)] + 1)
+            {
+                has_restart = true;
+            }
+        }
     }
 
-    long long result = (long long)n - a[m - 1] + 1;
-    cout << result << endl;
+    if (has_restart)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        long long result = n - a[static_cast<size_t>(m - 1)] + 1;
+        cout << result << endl;
+    }
 }
 
 int main()
 {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
-    cin >> t;
+    if (!(cin >> t))
+        return 0;
     while (t--)
     {
         solve();
