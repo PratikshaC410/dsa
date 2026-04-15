a7bbe145d965fb643c46e5a882ef87911031e468
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-15 22:05:26 +0530

    leaders of each array element

---
+++ ARRAYS/leadersofarr.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> leaders(vector<int> &nums)
+    {
+        vector<int> ans;
+
+        if (nums.empty())
+        {
+            return ans;
+        }
+
+        int max = nums[nums.size() - 1];
+        ans.push_back(nums[nums.size() - 1]);
+
+        for (int i = nums.size() - 2; i >= 0; i--)
+        {
+            if (nums[i] > max)
+            {
+                ans.push_back(nums[i]);
+                max = nums[i];
+            }
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};
+
+int main()
+{
+    vector<int> nums = {10, 22, 12, 3, 0, 6};
+
+    Solution finder;
+
+    vector<int> ans = finder.leaders(nums);
+
+    cout << "Leaders in the array are: ";
+    for (int i = 0; i < ans.size(); i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
