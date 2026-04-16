84780e6ad383ffac4d8ca210b655430e839765f7
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-16 23:27:01 +0530

    search in binary search tree

---
+++ bst/searchinbst.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition of TreeNode
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int data)
+    {
+        val = data;
+        left = right = nullptr;
+    }
+};
+
+class Solution
+{
+public:
+    TreeNode *searchBST(TreeNode *root, int target)
+    {
+
+        while (root != nullptr && root->val != target)
+        {
+
+            if (target < root->val)
+            {
+                root = root->left;
+            }
+
+            else
+            {
+                root = root->right;
+            }
+        }
+
+        return root;
+    }
+};
