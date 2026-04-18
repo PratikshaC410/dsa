08859c000c69f89269048c414ba25d609eecdf8e
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-18 21:15:56 +0530

    minimum seats allocating for the students

--- greedy/minseats.cpp
+++ greedy/minseats.cpp
@@ -1,62 +1,67 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <vector>
+
 using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    // Track which seats are already 'covered' (blocked)
+    vector<bool> covered(n, false);
+    int initial_students = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (s[i] == '1')
+        {
+            initial_students++;
+            covered[i] = true;
+            if (i > 0)
+                covered[i - 1] = true;
+            if (i < n - 1)
+                covered[i + 1] = true;
+        }
+    }
+
+    int added_students = 0;
+    for (int i = 0; i < n; i++)
+    {
+        // If we find a seat that is not covered by anyone...
+        if (!covered[i])
+        {
+            added_students++;
+            // Place a student at i+1 to cover i, i+1, and i+2
+            // If i is the very last seat, we just cover i.
+            int placement = i + 1;
+            if (placement >= n)
+                placement = i;
+
+            // Mark the new coverage
+            covered[placement] = true;
+            if (placement > 0)
+                covered[placement - 1] = true;
+            if (placement < n - 1)
+                covered[placement + 1] = true;
+        }
+    }
+
+    cout << initial_students + added_students << endl;
+}
 
 int main()
 {
-
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t;
     cin >> t;
-
     while (t--)
     {
-        int n;
-        string s;
-        cin >> n >> s;
-
-        int totalStudents = 0;
-
-        for (char c : s)
-        {
-            if (c == '1')
-                totalStudents++;
-        }
-
-        int i = 0;
-        while (i < n)
-        {
-            if (s[i] == '0')
-            {
-                int gapStart = i;
-                while (i < n && s[i] == '0')
-                {
-                    i++;
-                }
-                int gapLen = i - gapStart;
-
-                bool hasLeftNeighbor = (gapStart > 0 && s[gapStart - 1] == '1');
-                bool hasRightNeighbor = (i < n && s[i] == '1');
-                bool isMiddleGap = hasLeftNeighbor && hasRightNeighbor;
-
-                int toAdd;
-                if (isMiddleGap)
-                {
-                    toAdd = (gapLen - 1) / 2;
-                }
-                else
-                {
-                    toAdd = max(1, gapLen / 2);
-                }
-
-                totalStudents += toAdd;
-            }
-            else
-            {
-                i++;
-            }
-        }
-
-        cout << totalStudents << "\n";
+        solve();
     }
-
     return 0;
 }
