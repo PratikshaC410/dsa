c54321de8c0dde975945bdd1d9a307092ebfc4c0
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-20 14:20:39 +0530

    finding the middle of the node using tortoise and hare method

---
+++ linkedlist/midnodeinll.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+    Node *prev;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+        prev = nullptr;
+    }
+};
+
+Node *findmidnode(Node *&head, int val)
+{
+    Node *fast = head;
+    Node *slow = head;
+    while (fast != NULL && fast->next != NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
+    }
+
+    return slow;
+}
