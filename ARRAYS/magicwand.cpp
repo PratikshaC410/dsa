52678cef17c3bfb1d8503ddc47f3e059ff24904c
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-27 18:01:50 +0530

    coutning the number of operations for odd and even parity

--- ARRAYS/magicwand.cpp
+++ ARRAYS/magicwand.cpp
@@ -1,7 +1,6 @@
-#include <bits/stdc++.h>
-using namespace std;
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -10,51 +9,42 @@ void solve()
     int n;
     cin >> n;
     vector<int> a(n);
-    for (int i = 0; i < n; ++i)
-        cin >> a[i];
-
-    int e = 0; // Pointer for even numbers
-    int o = 0; // Pointer for odd numbers
+    bool has_even = false;
+    bool has_odd = false;
 
     for (int i = 0; i < n; ++i)
     {
-        // Advance e to the next even number
-        while (e < n && (a[e] & 1))
-            e++;
-        // Advance o to the next odd number
-        while (o < n && !(a[o] & 1))
-            o++;
-
-        if (e < n && o < n)
-        {
-            if (a[e] < a[o])
-            {
-                cout << a[e++] << " ";
-            }
-            else
-            {
-                cout << a[o++] << " ";
-            }
-        }
-        else if (e < n)
-        {
-            cout << a[e++] << " ";
-        }
-        else if (o < n)
-        {
-            cout << a[o++] << " ";
-        }
+        cin >> a[i];
+        if (a[i] % 2 == 0)
+            has_even = true;
+        else
+            has_odd = true;
     }
-    cout << '\n';
+
+    // If both parities exist, the entire array can be sorted
+    // lexicographically because different parities act as swap bridges.
+    if (has_even && has_odd)
+    {
+        sort(a.begin(), a.end());
+    }
+
+    for (int i = 0; i < n; ++i)
+    {
+        cout << a[i] << (i == n - 1 ? "" : " ");
+    }
+    cout << "\n";
 }
 
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--)
+    {
         solve();
+    }
     return 0;
 }
