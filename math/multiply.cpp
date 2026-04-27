44c951afe994da965b39a69e22348c094c5849a1
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-27 18:11:52 +0530

    min multiplication so make a=b

---
+++ math/multiply.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+
+    if (a == b)
+    {
+        cout << 0 << endl;
+    }
+    else if (a % b == 0 || b % a == 0)
+    {
+
+        cout << 1 << endl;
+    }
+    else
+    {
+        cout << 2 << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
