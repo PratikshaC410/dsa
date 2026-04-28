ef07c1c426ce2aee69c1977cf017feb126f5b97c
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-28 18:47:12 +0530

    reverse the operations

---
+++ math/conjecture.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void solve()
+{
+    long long k, x;
+    cin >> k >> x;
+    long long initial_value = x;
+
+    for (int i = 0; i < k; ++i)
+    {
+        initial_value *= 2;
+    }
+
+    cout << initial_value << endl;
+}
+
+int main()
+{
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
