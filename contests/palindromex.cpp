168ea15153c0f511003de57eff39ee5836823439
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-30 21:24:38 +0530

    different approach

--- contests/palindromex.cpp
+++ contests/palindromex.cpp
@@ -8,37 +8,70 @@ void solve()
 {
     int n;
     cin >> n;
-    vector<int> a(2 * n);
     vector<vector<int>> pos(n);
     for (int i = 0; i < 2 * n; i++)
     {
-        cin >> a[i];
-        pos[a[i]].push_back(i);
+        int x;
+        cin >> x;
+        pos[x].push_back(i);
     }
 
-    int max_mex = 1;
+    int ans = 1;
 
     int L = pos[0][0], R = pos[0][1];
-    int current_k = 1;
 
-    while (current_k < n)
+    for (int i = 1; i < n; i++)
     {
-        bool center_found = false;
-        // Can current_k be a center for the mirrored 0...current_k-1?
-        // This requires 0...current_k-1 to be mirrored relative to one pos of current_k
-        int p1 = pos[current_k][0];
-        int p2 = pos[current_k][1];
-
-        // Logic: If 0...current_k-1 are mirrored around p1, MEX is current_k + 1
-        // (Checking mirroring requires tracking the L and R of the mirrored block)
-
-        // Simple case from image: if both 0s surround a 1, MEX is 2.
-        // If both 0s and both 1s surround a 2, MEX is 3.
-
-        // For the sake of this placement prep, the core logic is:
-        // MEX = K if (0...K-2) are nested and (K-1) is in the middle.
-
-        // This loop would verify the nesting property for each increasing MEX.
-        break;
+        if (pos[i][0] > L && pos[i][0] < R)
+            ans = max(ans, 2);
+        if (pos[i][1] > L && pos[i][1] < R)
+            ans = max(ans, 2);
     }
+
+    int current_L = L, current_R = R;
+    int k = 1;
+    while (k < n)
+    {
+        int p1 = pos[k][0], p2 = pos[k][1];
+
+        if (p1 < current_L && p2 > current_R)
+        {
+            if (current_L - p1 == p2 - current_R)
+            {
+                current_L = p1;
+                current_R = p2;
+                k++;
+
+                ans = max(ans, k);
+            }
+            else
+                break;
+        }
+        else
+            break;
+    }
+
+    if (k < n)
+    {
+        if ((pos[k][0] > current_L && pos[k][0] < current_R) ||
+            (pos[k][1] > current_L && pos[k][1] < current_R))
+        {
+            ans = max(ans, k + 1);
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
 }
