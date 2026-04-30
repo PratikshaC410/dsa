05914e178f45d8effbf30ef71e0b1ec655bf1f6d
PratikshaC410 <pratiksha42719@gmail.com>
2026-04-30 22:10:49 +0530

    good subarrya using stack

--- contests/goodsubarr.cpp
+++ contests/goodsubarr.cpp
@@ -1,61 +1,60 @@
 #include <iostream>
 #include <vector>
-#include <map>
+#include <stack>
 
 using namespace std;
+
+typedef long long ll;
 
 void solve()
 {
     int n;
     cin >> n;
-    vector<long long> a(n + 1);
-    vector<long long> p(n + 1, 0);
-
-    for (int i = 1; i <= n; ++i)
-    {
+    vector<ll> a(n);
+    for (int i = 0; i < n; i++)
         cin >> a[i];
-        if (i % 2 == 1)
-        {
-            p[i] = p[i - 1] + a[i];
-        }
-        else
-        {
-            p[i] = p[i - 1] - a[i];
-        }
-    }
 
-    long long count = 0;
-    map<long long, int> even_indices;
-    map<long long, int> odd_indices;
+    ll total_good = 0;
 
-    even_indices[0] = 1;
-
-    for (int i = 1; i <= n; ++i)
+    for (int l = 0; l < n; l++)
     {
-        if (i % 2 == 0)
+        vector<ll> s;
+        for (int r = l; r < n; r++)
         {
-            count += even_indices[p[i]];
-            even_indices[p[i]]++;
-        }
-        else
-        {
-            count += odd_indices[p[i]];
-            odd_indices[p[i]]++;
+            s.push_back(a[r]);
+
+            while (s.size() >= 3)
+            {
+                int m = s.size();
+                if (s[m - 3] + s[m - 1] > s[m - 2])
+                {
+                    ll x = s[m - 3] - s[m - 2] + s[m - 1];
+                    s.pop_back();
+                    s.pop_back();
+                    s.pop_back();
+                    s.push_back(x);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            if (s.size() == 1)
+            {
+                total_good++;
+            }
         }
     }
-
-    cout << count << "\n";
+    cout << total_good << "\n";
 }
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
     int t;
     cin >> t;
     while (t--)
-    {
         solve();
-    }
     return 0;
 }
