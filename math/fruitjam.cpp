582550b499d5547594e7033a50cd14569c225a9a
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-06 21:20:43 +0530

    number of fruits req to prep given amt of jam

---
+++ math/fruitjam.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    long long n;
+    cin >> n;
+    cout << 2 * n << endl;
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
