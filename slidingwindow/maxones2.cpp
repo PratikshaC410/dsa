9891657de297321d19db1fc19f181b4250685550
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-06 22:13:57 +0530

    finding the max consecutive ones by flipping atmost k zeros

---
+++ slidingwindow/maxones2.cpp
@@ -0,0 +1,31 @@
+class Solution
+{
+public:
+    int maxOnes(vector<int> &arr, int k)
+    {
+        int left = 0;
+        int right = 0;
+        int zeroCount = 0;
+        int maxLen = 0;
+
+        for (right = 0; right < arr.size(); ++right)
+        {
+
+            if (arr[right] == 0)
+            {
+                zeroCount++;
+            }
+            while (zeroCount > k)
+            {
+                if (arr[left] == 0)
+                {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLen = std::max(maxLen, right - left + 1);
+        }
+
+        return maxLen;
+    }
+};
