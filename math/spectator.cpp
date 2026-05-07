556c97f058387264a2645bc732324dac57351cec
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-07 21:20:32 +0530

    math game

--- math/spectator.cpp
+++ math/spectator.cpp
@@ -5,8 +5,12 @@ using namespace std;
 void solve()
 {
     long long k;
-    cin >> k;
-    if (k % 2 != 0)
+    if (!(cin >> k))
+        return;
+
+    // The spectator repeats every 3 matches
+    // Match 1, 4, 7, 10... (k % 3 == 1)
+    if (k % 3 == 1)
     {
         cout << "YES" << endl;
     }
@@ -18,6 +22,10 @@ void solve()
 
 int main()
 {
+    // Optimize I/O for competitive programming
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
     int t;
     cin >> t;
     while (t--)
