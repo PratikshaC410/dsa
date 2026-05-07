ab098d4964bb4ffb8deae5c6f432151f2388a188
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-07 21:17:21 +0530

    math game

---
+++ math/spectator.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+void solve()
+{
+    long long k;
+    cin >> k;
+    if (k % 2 != 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
