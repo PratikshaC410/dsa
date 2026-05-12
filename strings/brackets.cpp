ea2f083a38a2f1d5369fc3ab384cb10b82e0a855
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-12 23:50:15 +0530

    checking the balanced and unbalanced brackets

--- strings/brackets.cpp
+++ strings/brackets.cpp
@@ -3,20 +3,40 @@
 #include <vector>
 
 using namespace std;
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
 
 void solve()
 {
     string s;
     cin >> s;
-
     int n = s.length();
-    string target = "";
 
-    for (int i = 0; i < n / 2; ++i)
+    // Check if the string is just one deep nest: ((...))
+    // We can check this by seeing if the sequence remains "open"
+    // until the very last character.
+    int balance = 0;
+    bool depth_greater_than_one = false;
+    bool is_simple_nest = true;
+
+    for (int i = 0; i < n - 1; ++i)
     {
-        target += "()";
+        if (s[i] == '(')
+            balance++;
+        else
+            balance--;
+
+        // If balance hits 0 before the end, it's not a single simple nest (like ()())
+        if (balance == 0)
+        {
+            is_simple_nest = false;
+        }
     }
-    if (s == target)
+
+    if (is_simple_nest)
     {
         cout << "NO" << endl;
     }
