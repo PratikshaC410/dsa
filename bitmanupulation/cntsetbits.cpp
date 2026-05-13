7abf931395fb57f0f2fcac65453d6cc24bb64448
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-13 18:08:48 +0530

    couting the set bits in number

---
+++ bitmanupulation/cntsetbits.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    int countSetBits(int n)
+    {
+        int count = 0;
+
+        while (n > 0)
+        {
+            count += (n & 1);
+            n >>= 1;
+        }
+
+        return count;
+    }
+};
+
+int main()
+{
+    int n = 29;
+
+    Solution sol;
+    int result = sol.countSetBits(n);
+
+    cout << "The number of set bits is: " << result << endl;
+
+    return 0;
+}
