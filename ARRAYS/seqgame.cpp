387000b45fd7db9408edabc72d3fd86241dd9b96
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-20 22:43:45 +0530

    sequence game

--- ARRAYS/seqgame.cpp
+++ ARRAYS/seqgame.cpp
@@ -1,60 +1,37 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <map>
-
+#include <bits/stdc++.h>
 using namespace std;
-
-void solve()
-{
-    int n;
-    cin >> n;
-    vector<int> a(n);
-    map<int, int> counts;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-        counts[a[i]]++;
-    }
-
-    if (n == 3)
-    {
-        if (counts.size() == 1)
-        {
-            cout << "YES\n";
-        }
-        else
-        {
-            cout << "NO\n";
-        }
-        return;
-    }
-
-    int max_freq = 0;
-    for (auto const &[val, count] : counts)
-    {
-        max_freq = max(max_freq, count);
-    }
-
-    if (max_freq >= (n + 1) / 2)
-    {
-        cout << "YES\n";
-    }
-    else
-    {
-        cout << "NO\n";
-    }
-}
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
     int t;
     cin >> t;
+
     while (t--)
     {
-        solve();
+        int n;
+        cin >> n;
+
+        map<int, int> freq;
+
+        for (int i = 0; i < n; i++)
+        {
+            int x;
+            cin >> x;
+            freq[x]++;
+        }
+
+        int mx = 0;
+        for (auto &[v, f] : freq)
+            mx = max(mx, f);
+
+        if (mx <= (n + 1) / 2)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
     }
+
     return 0;
 }
