ad9a9e3db8b6de9ea569b3cae052316ae72f7e13
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-24 22:31:58 +0530

    discoint counting on itmes

--- greedy/discount.cpp
+++ greedy/discount.cpp
@@ -1,70 +1,70 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+
+    vector<long long> a(n);
+    long long total_sum = 0;
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> a[i];
+        total_sum += a[i];
+    }
+
+    vector<int> b(k);
+    for (int i = 0; i < k; ++i)
+    {
+        cin >> b[i];
+    }
+
+    // Step 1: Sort products in descending order
+    sort(a.begin(), a.end(), greater<long long>());
+
+    // Step 2: Sort vouchers in ascending order
+    sort(b.begin(), b.end());
+
+    long long total_discount = 0;
+    int current_idx = 0;
+
+    // Step 3: Match vouchers to elements greedily
+    for (int i = 0; i < k; ++i)
+    {
+        int voucher_size = b[i];
+
+        // If there aren't enough products left to satisfy the voucher size, we stop
+        if (current_idx + voucher_size > n)
+        {
+            break;
+        }
+
+        // The cheapest item in this optimal group of 'voucher_size' elements
+        // will be at index (current_idx + voucher_size - 1)
+        total_discount += a[current_idx + voucher_size - 1];
+
+        // Consume these elements
+        current_idx += voucher_size;
+    }
+
+    cout << total_sum - total_discount << "\n";
+}
 
 int main()
 {
+    // Optimize standard I/O operations for competitive programming
     ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
+    cin.tie(NULL);
 
     int t;
     cin >> t;
-
     while (t--)
     {
-        int n, k;
-        cin >> n >> k;
-
-        vector<long long> a(n);
-        for (int i = 0; i < n; i++)
-        {
-            cin >> a[i];
-        }
-
-        vector<int> b(k);
-        for (int i = 0; i < k; i++)
-        {
-            cin >> b[i];
-        }
-
-        // Sort products in ASCENDING order (cheapest first)
-        sort(a.begin(), a.end());
-
-        // Sort vouchers in DESCENDING order (largest first)
-        sort(b.begin(), b.end(), greater<int>());
-
-        long long total_cost = 0;
-        int idx = 0;
-
-        // Apply each voucher
-        for (int i = 0; i < k && idx < n; i++)
-        {
-            int voucher_size = b[i];
-
-            if (idx + voucher_size <= n)
-            {
-                // Skip the cheapest product (index idx), pay for rest
-                idx++; // skip cheapest
-                for (int j = 0; j < voucher_size - 1; j++)
-                {
-                    total_cost += a[idx];
-                    idx++;
-                }
-            }
-            else
-            {
-                // Can't use this voucher, move to next
-                continue;
-            }
-        }
-
-        // Pay for remaining products
-        while (idx < n)
-        {
-            total_cost += a[idx];
-            idx++;
-        }
-
-        cout << total_cost << "\n";
+        solve();
     }
 
     return 0;
