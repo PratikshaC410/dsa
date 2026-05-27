0c6273cb76142fed863bc6e418bc64dab33a655f
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-27 19:08:05 +0530

    even modulo pair changes

--- math/evenmodpair.cpp
+++ math/evenmodpair.cpp
@@ -1,45 +1,83 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-
+#include <bits/stdc++.h>
 using namespace std;
 
-void solve()
+int main()
 {
-    int n;
-    cin >> n;
-    vector<long long> a(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> a[i];
-    }
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
 
-    int limit = min(n, 4);
+    int t;
+    cin >> t;
 
-    for (int i = 0; i < limit; i++)
+    while (t--)
     {
-        for (int j = i + 1; j < limit; j++)
+        int n;
+        cin >> n;
+
+        vector<long long> a(n);
+        vector<long long> even;
+        bool hasOne = false;
+
+        for (int i = 0; i < n; i++)
         {
-            if ((a[j] % a[i]) % 2 == 0)
+            cin >> a[i];
+            if (a[i] == 1)
+                hasOne = true;
+            if (a[i] % 2 == 0)
+                even.push_back(a[i]);
+        }
+
+        if (hasOne)
+        {
+            cout << 1 << " " << a[1] << '\n';
+        }
+        else if (even.size() >= 2)
+        {
+            cout << even[0] << " " << even[1] << '\n';
+        }
+        else
+        {
+            bool found = false;
+
+            if (even.size() == 1)
             {
-                cout << a[i] << " " << a[j] << "\n";
-                return;
+                long long y = even[0];
+                for (int i = 0; i < n; i++)
+                {
+                    if (a[i] < y && (y / a[i]) % 2 == 0)
+                    {
+                        cout << a[i] << " " << y << '\n';
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                for (int i = 0; i < n; i++)
+                {
+                    if (i + 1 < n && (a[i + 1] % a[i]) % 2 == 0)
+                    {
+                        cout << a[i] << " " << a[i + 1] << '\n';
+                        found = true;
+                        break;
+                    }
+                    if (i + 2 < n && (a[i + 2] % a[i]) % 2 == 0)
+                    {
+                        cout << a[i] << " " << a[i + 2] << '\n';
+                        found = true;
+                        break;
+                    }
+                }
+            }
+
+            if (!found)
+            {
+                cout << -1 << '\n';
             }
         }
     }
 
-    cout << -1 << "\n";
-}
-
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        solve();
-    }
     return 0;
 }
