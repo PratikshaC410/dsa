c9d5807017e73805ac2b4ff09ef139da304db146
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-30 16:25:53 +0530

    array operations using greedy

---
+++ math/dream.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool valid(int p, int q)
+{
+    return p <= 2 * (q + 1) && q <= 2 * (p + 1);
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int a, b, c, d;
+        cin >> a >> b >> c >> d;
+
+        if (valid(a, b) && valid(c - a, d - b))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+    return 0;
+}
