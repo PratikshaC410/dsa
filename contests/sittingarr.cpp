c6ee37bda42cc3e928ea14655b40b4466368c3a5
PratikshaC410 <pratiksha42719@gmail.com>
2026-05-31 10:18:12 +0530

    finding the seating arrangements

--- contests/sittingarr.cpp
+++ contests/sittingarr.cpp
@@ -11,33 +11,57 @@ void solve()
     cin >> n >> x >> s;
     string u;
     cin >> u;
+
     vector<int> dp(x + 1, -1);
     dp[0] = 0;
 
+    int min_j = 0;
+    int max_j = 0;
+
     for (int i = 0; i < n; i++)
     {
-        vector<int> next_dp = dp;
-        char personality = u[i];
+        char p = u[i];
 
-        for (int j = 0; j <= x; j++)
+        int prev_max_j = max_j;
+        if (p == 'I' && max_j < x)
         {
-            if (dp[j] == -1)
-                continue;
-
-            int seated = dp[j];
-            int available_seats = (j * s) - seated;
-
-            if ((personality == 'I' || personality == 'A') && j < x)
-            {
-                next_dp[j + 1] = max(next_dp[j + 1], seated + 1);
-            }
-
-            if ((personality == 'E' || personality == 'A') && available_seats > 0)
-            {
-                next_dp[j] = max(next_dp[j], seated + 1);
-            }
+            max_j++;
         }
-        dp = move(next_dp);
+        else if (p == 'A' && max_j < x)
+        {
+            max_j++;
+        }
+
+        for (int j = max_j; j >= min_j; j--)
+        {
+            int res = dp[j];
+
+            if (j > 0 && (p == 'I' || p == 'A'))
+            {
+                if (dp[j - 1] != -1)
+                {
+                    res = max(res, dp[j - 1] + 1);
+                }
+            }
+            if (p == 'E' || p == 'A')
+            {
+                if (dp[j] != -1)
+                {
+                    int available_seats = (j * s) - dp[j];
+                    if (available_seats > 0)
+                    {
+                        res = max(res, dp[j] + 1);
+                    }
+                }
+            }
+
+            dp[j] = res;
+        }
+
+        while (min_j <= max_j && dp[min_j] == -1)
+        {
+            min_j++;
+        }
     }
 
     int max_seated = 0;
