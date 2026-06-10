7082d6c3ba5f3ad1d2cfb516d5921ee7d158b704
PratikshaC410 <pratiksha42719@gmail.com>
2026-06-10 23:21:19 +0530

    game of flipping coins

---
+++ math/game.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    int u_count = 0;
+    for (char c : s)
+    {
+        if (c == 'U')
+        {
+            u_count++;
+        }
+    }
+
+    if (u_count % 2 != 0)
+    {
+        cout << "YES\n";
+    }
+    else
+    {
+        cout << "NO\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
