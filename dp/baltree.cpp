185355245fcb6be4d14c96ba6607da5f843ff5fb
PratikshaC410 <pratiksha42719@gmail.com>
2026-06-16 22:50:21 +0530

    balance tree with changes

--- dp/baltree.cpp
+++ dp/baltree.cpp
@@ -24,28 +24,26 @@ int main()
         vector<int> x(m);
         for (int i = 0; i < m; i++)
             cin >> x[i];
+
         priority_queue<long long> pq[2];
         for (int i = 1; i <= n; i++)
             pq[i & 1].push(a[i]);
+
         long long marked = 0;
 
-        int need[2] = {0, 0};
+        bool has_marked[2] = {false, false};
 
-        while (!pq[0].empty())
-            pq[0].pop();
-        while (!pq[1].empty())
-            pq[1].pop();
-        for (int i = 1; i <= n; i++)
-            pq[i & 1].push(a[i]);
-
-        marked = 0;
         for (int i = 0; i < m; i++)
         {
             int p = x[i] & 1;
             if (!pq[p].empty())
             {
-                marked += pq[p].top();
-                pq[p].pop();
+                if (!has_marked[p] || pq[p].top() > 0)
+                {
+                    marked += pq[p].top();
+                    pq[p].pop();
+                    has_marked[p] = true;
+                }
             }
         }
 
